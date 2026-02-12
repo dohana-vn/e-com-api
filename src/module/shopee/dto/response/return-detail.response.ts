aa791578df79776aad1d6b7eb5599cd8dded9ca5
Nguyen Duc Anh <97099707+anhhducnguyen@users.noreply.github.com>
2026-02-12 03:11:59 +0000

    feat: eslint

--- src/module/shopee/dto/response/return-detail.response.ts
+++ src/module/shopee/dto/response/return-detail.response.ts
@@ -94,16 +94,6 @@ export interface ShopeeResponseGetReturnDetail {
   response?: ShopeeReturnDetail;
 }
 
-// export interface ShopeeReturnItem {
-//   return_sn: string;
-//   return_id: number;
-//   order_sn: string;
-//   status: string;
-//   negotiation_status: string;
-//   create_time: number;
-//   update_time: number;
-// }
-
 export interface ShopeeResponseGetReturnList {
   response: {
     more: boolean;
