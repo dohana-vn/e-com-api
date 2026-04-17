9921e0489c3654d1d7b101b7e69d3b7003877058
Nguyen Duc Anh <anhnguyen2k373@gmail.com>
2026-04-17 16:02:46 +0000

    feat: add getShopInfo function and related types for Shopee API

--- src/module/shopee/dto/response/config.response.ts
+++ src/module/shopee/dto/response/config.response.ts
@@ -21,6 +21,21 @@ interface ShopProfile {
   invoice_issuer: string;
 }
 
+interface ShopInfoSipAffiliateShop {
+  affi_shop_id: number;
+  region: string;
+  is_cb: boolean;
+}
+
+interface ShopInfoLinkedDirectShop {
+  direct_shop_id: number;
+  direct_shop_region: string;
+}
+
+interface ShopInfoOutletShop {
+  outlet_shop_id: number;
+}
+
 interface ResponseCommon<T> {
   request_id: string;
   error: string;
@@ -30,9 +45,39 @@ interface ResponseCommon<T> {
 
 interface ResponseShopProfile extends ResponseCommon<ShopProfile> {}
 
+interface ResponseShopInfo {
+  request_id: string;
+  error: string;
+  message: string;
+  auth_time: number;
+  expire_time: number;
+  shop_name: string;
+  region: string;
+  status: string;
+  shop_fulfillment_flag?: string;
+  is_cb?: boolean;
+  is_upgraded_cbsc?: boolean;
+  merchant_id: number | null;
+  is_sip?: boolean;
+  sip_affi_shops?: ShopInfoSipAffiliateShop[];
+  is_main_shop?: boolean;
+  is_direct_shop?: boolean;
+  linked_main_shop_id?: number | null;
+  linked_direct_shop_list?: ShopInfoLinkedDirectShop[];
+  is_one_awb?: boolean;
+  is_mart_shop?: boolean;
+  is_outlet_shop?: boolean;
+  mart_shop_id?: number;
+  outlet_shop_info_list?: ShopInfoOutletShop[];
+}
+
 export {
   ResponseCommon as ShopeeResponseCommon,
   ResponseGetAccessToken as ShopeeResponseGetAccessToken,
   ResponseRefreshAccessToken as ShopeeResponseRefreshAccessToken,
   ResponseShopProfile as ShopeeResponseShopProfile,
+  ResponseShopInfo as ShopeeResponseShopInfo,
+  ShopInfoSipAffiliateShop as ShopeeShopInfoSipAffiliateShop,
+  ShopInfoLinkedDirectShop as ShopeeShopInfoLinkedDirectShop,
+  ShopInfoOutletShop as ShopeeShopInfoOutletShop,
 };
